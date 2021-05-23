abd4a02d9992cb81f3d523d309f911cedb57efb4
mdawn <mistydawncarmichael@gmail.com>
2021-05-23 18:07:06 -0500

    add logrus

--- go.mod
+++ go.mod
@@ -4,5 +4,6 @@ go 1.16
 
 require (
 	github.com/aclements/go-moremath v0.0.0-20210112150236-f10218a38794 // indirect
+	github.com/sirupsen/logrus v1.8.1 // indirect
 	github.com/urfave/cli v1.22.5
 )
--- main.go
+++ main.go
@@ -2,17 +2,22 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
-	"github.com/urfave/cli"
 	"io/ioutil"
-	"log"
 	"math"
 	"net/http"
 	"os"
 	"strconv"
 
+	"github.com/urfave/cli"
 	"github.com/aclements/go-moremath/stats"
+	log "github.com/sirupsen/logrus"
 )
+
+func init() {
+	// Log as JSON instead of the default ASCII formatter.
+	log.SetFormatter(&log.TextFormatter{FullTimestamp: true})
+  
+  }
 
 func info() {
 	app.Name = "Alerting Tool"
@@ -53,24 +58,26 @@ func commands() {
 			Aliases: []string{"b"},
 			Usage:   "Ticker for BTCUSD pair",
 			Action: func(c *cli.Context) {
+				log.Println("Getting BTCUSD Ticker API Data")
 				resp, err := http.Get("https://api.gemini.com/v2/ticker/btcusd")
 				if err != nil {
-					fmt.Println("Error:", err)
-					os.Exit(1)
+					log.Error(err)
 				}
 
 				responseData, err := ioutil.ReadAll(resp.Body)
+				log.Println("Reading Response")
 				if err != nil {
 					log.Fatal(err)
 				}
 
 				var responseObject Ticker
 				json.Unmarshal(responseData, &responseObject)
+				log.Println("Parsing Response")
 
-				fmt.Println("Open price: ", responseObject.Open)
-				fmt.Println("High price: ", responseObject.High)
-				fmt.Println("Low price: ", responseObject.Low)
-				fmt.Println("Hourly prices per last 24 hours :", responseObject.Changes)
+				log.Println("Open price: ", responseObject.Open)
+				log.Println("High price: ", responseObject.High)
+				log.Println("Low price: ", responseObject.Low)
+				log.Println("Hourly prices per last 24 hours :", responseObject.Changes)
 			},
 		},
 		{
@@ -78,43 +85,22 @@ func commands() {
 			Aliases: []string{"c"},
 			Usage:   "Get current price for BTCUSD pair",
 			Action: func(c *cli.Context) {
+				log.Println("Getting Pricefeed API Data")
 				resp, err := http.Get("https://api.gemini.com/v1/pricefeed")
 
 				responseData, err := ioutil.ReadAll(resp.Body)
+				log.Println("Reading Response")
 				if err != nil {
 					log.Fatal(err)
 				}
 
-				currentPriceFake := 4000
-				fmt.Println("Current price: ", currentPriceFake)
+				currentPriceFake := 4000.00
+				log.Println("Current price: ", currentPriceFake)
 
 				var responseObject Pricefeed
 				json.Unmarshal(responseData, &responseObject)
-				fmt.Println(responseObject)
-			},
-		},
-		{
-			Name:    "deviation",
-			Aliases: []string{"d"},
-			Usage:   "Get standard deviation for BTCUSD pair",
-			Action: func(c *cli.Context) {
-				resp, err := http.Get("https://api.gemini.com/v2/ticker/btcusd")
-				if err != nil {
-					fmt.Println("Error:", err)
-					os.Exit(1)
-				}
-
-				responseData, err := ioutil.ReadAll(resp.Body)
-				if err != nil {
-					log.Fatal(err)
-				}
-
-				var responseObject Ticker
-				json.Unmarshal(responseData, &responseObject)
-				hourlies := ConvertSlice(responseObject.Changes)
-				deviation := StdDev(hourlies)
-
-				fmt.Println("Standard deviation: ", deviation)
+				log.Println("Parsing Response")
+				log.Println(responseObject)
 			},
 		},
 		{
@@ -122,23 +108,69 @@ func commands() {
 			Aliases: []string{"v"},
 			Usage:   "Get the average price for BTCUSD pair over the last 24 hours",
 			Action: func(c *cli.Context) {
+				log.Println("Getting BTCUSD Ticker API Data")
 				resp, err := http.Get("https://api.gemini.com/v2/ticker/btcusd")
 				if err != nil {
-					fmt.Println("Error:", err)
-					os.Exit(1)
+					log.Error(err)
 				}
 
 				responseData, err := ioutil.ReadAll(resp.Body)
+				log.Println("Reading Response")
 				if err != nil {
 					log.Fatal(err)
 				}
 
 				var responseObject Ticker
 				json.Unmarshal(responseData, &responseObject)
+				log.Println("Parsing Response")
 				hourlies := ConvertSlice(responseObject.Changes)
+				log.Println("Converting Data")
 				average := Average(hourlies)
+				log.Println("Calculating Average")
 
-				fmt.Println("Average: ", average)
+				log.Println("Average: ", average)
+			},
+		},
+		{
+			Name:    "deviation",
+			Aliases: []string{"d"},
+			Usage:   "Get standard deviation for BTCUSD pair",
+			Action: func(c *cli.Context) {
+				log.Println("Getting BTCUSD Ticker API Data")
+				resp, err := http.Get("https://api.gemini.com/v2/ticker/btcusd")
+				if err != nil {
+					log.Error(err)
+				}
+
+				responseData, err := ioutil.ReadAll(resp.Body)
+				log.Println("Reading Response")
+				if err != nil {
+					log.Fatal(err)
+				}
+
+				currentPriceFake := 4000.00
+				log.Println("Current price: ", currentPriceFake)
+
+				var responseObject Ticker
+				json.Unmarshal(responseData, &responseObject)
+				log.Println("Parsing Response")
+				hourlies := ConvertSlice(responseObject.Changes)
+				log.Println("Converting Data")
+				deviation := StdDev(hourlies)
+				log.Println("Calculating Average")
+				average := Average(hourlies)
+				
+				log.Println("Average: ", average)
+
+				log.Println("Standard deviation: ", deviation)
+
+				log.Println("Calculating deviation from Average")
+				if currentPriceFake > (average + deviation) {
+					log.Println("Current Price is > 1 StdDev from Avg")
+				}
+				if currentPriceFake < (average - deviation) {
+					log.Println("Current Price is < 1 StdDev from Avg")
+				}
 			},
 		},
 	}
@@ -155,7 +187,7 @@ func ConvertSlice(xs []string) (p []float64) {
 	for _, arg := range xs[1:] {
 		n, err := strconv.ParseFloat(arg, 64)
 		if err != nil {
-			panic(err)
+			log.Fatal(err)
 		}
 		p = append(p, n)
 	}
